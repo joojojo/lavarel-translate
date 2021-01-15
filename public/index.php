e94f69c6740b0e80acbeda47072e51704a58da33
mangatoon-zhaochong <zhaochong@mangatoon.mobi>
2021-01-15 14:13:10 +0800

    autoload

--- public/index.php
+++ public/index.php
@@ -3,16 +3,18 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+//定义框架开始时间常量
 define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
-| Check If Application Is Under Maintenance
+| 检查应用程序是否正在维护
 |--------------------------------------------------------------------------
 |
 | If the application is maintenance / demo mode via the "down" command we
 | will require this file so that any prerendered template can be shown
 | instead of starting the framework, which could cause an exception.
+| 如果应用程序通过“ down”命令处于 维护/演示模式，我们将需要引入此文件，以便可以显示任何预渲染的模板而不是启动框架，这可能会导致异常。
 |
 */
 
@@ -22,12 +24,15 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 
 /*
 |--------------------------------------------------------------------------
-| Register The Auto Loader
+| 注册自动加载器
 |--------------------------------------------------------------------------
 |
 | Composer provides a convenient, automatically generated class loader for
 | this application. We just need to utilize it! We'll simply require it
 | into the script here so we don't need to manually load our classes.
+
+| Composer为此应用程序提供了一个方便的，自动生成的类加载器。 我们只需要利用它！ 我们只需要在这里将其添加到脚本中即可，因此我们不需要手动加载类。
+| 完成自动加载功能
 |
 */
 
